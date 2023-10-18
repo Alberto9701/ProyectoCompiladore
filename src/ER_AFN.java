73c6fd0aacdd41b23f72d5ba0992783ec23a78f2
leay21 <yaelt143@gmail.com>
2023-10-18 07:05:51 -0600

    ExpresionesRegulares1

---
+++ src/ER_AFN.java
@@ -0,0 +1,99 @@
+import java.io.IOException;
+import java.util.IllegalFormatCodePointException;
+
+public class ER_AFN {
+    String ExpReg;
+    public AFN Result;
+    public AnalizLexico L;
+
+    public ER_AFN (String sigma, String FileAFD, int IdentifAFD) throws IOException {
+        ExpReg = sigma;
+        L = new AnalizLexico(ExpReg, FileAFD, IdentifAFD);
+    }
+
+    public void setExpresion (String sigma) {
+        ExpReg = sigma;
+        L.SetSigma(sigma);
+    }
+     public boolean IniConversion() {
+        int Token;
+        AFN f = new AFN();
+         if (E(f)) {
+             Token = L.yylex();
+             if (Token == 0) {
+                this.Result = f;
+                return true;
+            }
+         }
+         return false;
+     }
+
+     public boolean E (AFN f) {
+        if (T(f)) {
+            if (Ep(f)) {
+                return true;
+            }
+        }
+        return false;
+     }
+     public boolean Ep (AFN f) {
+        int Token;
+        AFN f2 = new AFN();
+        Token = L.yylex();
+        if (Token == 10) {
+            if (T(f2)) {
+                f.UnirAFN(f2);
+                if (Ep(f)) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        L.UndoToken();
+        return true;
+     }
+
+     boolean T(AFN f) {
+        if (C(f)) {
+            if (Tp(f)) {
+                return true;
+            }
+        }
+        return false;
+     }
+
+     public boolean Tp(AFN f) {
+        int Token;
+        AFN f2 = new AFN();
+        Token = L.yylex();
+        if (Token == 20) {
+            if (C(f2)) {
+                f.ConcAFN(f2);
+                if (Tp(f)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        L.UndoToken();
+        return true;
+     }
+
+     public boolean C (AFN f) {
+        if (F(f)) {
+            if (Cp(f)) {
+                return true;
+            }
+        }
+        return false;
+     }
+
+     public boolean Cp (AFN f) {
+        int Token;
+        Token = L.yylex();
+        switch (Token) {
+            case 10:
+                f.Cerr
+        }
+     }
+}
